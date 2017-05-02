145e19151f09c546763d402baa39a6683ea43e3c
Michael Koceja and Tyler Allen <tyler@>
2017-05-02 11:09:06 -0700

    add comments to js

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,21 @@ $(function(){
     var userInput = $("input").val();
     var vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"];
 
+    //Change string into an array
     userInput = userInput.split("");
 
+    //Replace vowels with "-" with a loop
     for (var index = 0; index < userInput.length; index += 1) {
       if (vowels.includes(userInput[index])) {
         userInput[index] = "-";
       }
     }
 
+    //Change array back to a string
     userInput = userInput.join("");
 
+
+    //Show outcomes
     $("#output").show();
     $("#output").text(userInput);
   });
